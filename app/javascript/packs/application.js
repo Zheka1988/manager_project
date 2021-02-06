d96d1ab02a53cb47cda355cbf244ba866170951c
Zheka1988 <kveladze.kz@mail.ru>
2021-02-06 05:43:55 -0800

    установлен bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,7 +10,23 @@ import "channels"
 import "jquery"
 import 'common/tasks'
 import 'common/projects'
+import * as bootstrap from 'bootstrap'
+import "../stylesheets/application"
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
